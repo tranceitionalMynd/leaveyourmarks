d80c5046bbe74818d7cb62d2d7748327f94990f2
Tyler Cardon <tyler.k.cardon@gmail.com>
2017-02-20 16:51:52 -0600

    Revert "Added debugging."
    This reverts commit ab427a6e8ad6771ffa515ee714033a17878527e2.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express=require("express")();
-console.log("1");
 var static=require("express").static;
 var routes=require("./routes/routes");
 var user=require("./routes/user");
@@ -8,14 +7,12 @@ var bodyParser=require("body-parser");
 var session=require("express-session");
 var chalk=require("chalk");
 var db=require("./models/db.js")
-console.log("2");
 
 express.set("view engine", "ejs");
 express.use(static(__dirname + "/public/"));
 express.use(bodyParser.json());
 express.use(bodyParser.urlencoded({extended:false}));
 express.use(session({secret:"qazwsxedcrfvtgbyhnujm", resave: true, saveUninitialized: true}));
-console.log("3");
 
 express.use(function(error, request, result, next) {
     console.log(chalk.red("Error: 404"));
@@ -25,7 +22,6 @@ express.use(function(error, request, result, next) {
     console.log(chalk.red("Error 500"));
     result.status(500).render("500");
 });
-console.log("4");
 
 express.get('/', routes.index);
 express.get('/login', routes.login);
@@ -36,14 +32,12 @@ express.get('/techstack', routes.techStack);
 express.get('/story', routes.story);
 express.get('/stories', story.stories);
 express.get('/stories/:story', story.getStory);
-//express.get(p'/newUser', routes.newUser);
-console.log("5");
+//express.get('/newUser', routes.newUser);
 
 express.post('/newUser', user.doCreate);
 express.post('/authenticate', routes.authenticate);
 express.post('/add-story', story.addStory);
 express.post('/stories/:slug/saveComment', story.saveComment);
-console.log("6");
 
 // TODO: 404, 500, home, index, story
 
@@ -51,5 +45,3 @@ var port = process.env.PORT || 8080;
 var server = express.listen(port, function(request, response) {
    console.log(chalk.green("Listening on port: " + port));
 });
-
-console.log("7");
